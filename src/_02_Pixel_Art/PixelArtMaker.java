3f2de9df65dcf4639819153b8a8686e229d911ba
SeahawksRawesome <btpowell66@cox.net>
2020-06-14 17:37:37 -0700

    FINISHED IT
    YAY

--- src/_02_Pixel_Art/PixelArtMaker.java
+++ src/_02_Pixel_Art/PixelArtMaker.java
@@ -1,27 +1,46 @@
 package _02_Pixel_Art;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class PixelArtMaker implements MouseListener{
+
+
+public class PixelArtMaker implements MouseListener, ActionListener{
 	private JFrame window;
 	private GridInputPanel gip;
 	private GridPanel gp;
 	ColorSelectionPanel csp;
+	//private JPanel p1;
+	private JButton save;
+	private JButton load;
+	private static final String DATA_FILE = "src/_02_Pixel_Art/saved.dat";
 	
 	public void start() {
 		gip = new GridInputPanel(this);	
 		window = new JFrame("Pixel Art");
 		window.setLayout(new FlowLayout());
 		window.setResizable(false);
-		
+		save = new JButton();
+		load = new JButton();
 		window.add(gip);
 		window.pack();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
+		
+		
 	}
 
 	public void submitGridData(int w, int h, int r, int c) {
@@ -30,9 +49,25 @@ public class PixelArtMaker implements MouseListener{
 		window.remove(gip);
 		window.add(gp);
 		window.add(csp);
+		window.add(save);
+		window.add(load);
 		gp.repaint();
 		gp.addMouseListener(this);
+		
+		save.setVisible(true);
+		save.setSize(100, 50);
+		save.setText("save");
+		save.addActionListener(this);
+		save.setLocation(400, 300);
+		
+		load.setVisible(true);
+		load.setSize(100, 50);
+		load.setText("load");
+		load.addActionListener(this);
+		load.setLocation(600, 300);
+		
 		window.pack();
+		
 	}
 	
 	public static void main(String[] args) {
@@ -61,5 +96,38 @@ public class PixelArtMaker implements MouseListener{
 
 	@Override
 	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == save) {
+			
+			try (FileOutputStream fos = new FileOutputStream(new File(DATA_FILE)); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+				oos.writeObject(gp);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		if(e.getSource() == load) {
+			window.remove(gp);
+		gp =	load();
+			window.add(gp);
+			window.pack();
+		}
+	}
+	
+	private static GridPanel load() {
+		try (FileInputStream fis = new FileInputStream(new File(DATA_FILE)); ObjectInputStream ois = new ObjectInputStream(fis)) {
+			return (GridPanel) ois.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			// This can occur if the object we read from the file is not
+			// an instance of any recognized class
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
